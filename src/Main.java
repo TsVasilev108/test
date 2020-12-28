8764000194b74532ecdac97a8d845e5519fa6eb1
Tsvetan Vasilev <tsvassilev108@gmail.com>
2020-12-28 11:25:09 +0200

    kjjkkjj

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test122");
         System.out.println("test8888");
+        System.out.println("test44");
     }
 }
